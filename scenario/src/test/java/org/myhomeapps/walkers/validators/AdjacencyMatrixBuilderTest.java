2cf52898aaca35a595c2b63b85f576001545c0d8
Squirrel994 <yarmol.olga@gmail.com>
2020-06-04 01:03:01 +0400

    add unit tests for DeadEndValidator

--- scenario/src/test/java/org/myhomeapps/walkers/validators/AdjacencyMatrixBuilderTest.java
+++ scenario/src/test/java/org/myhomeapps/walkers/validators/AdjacencyMatrixBuilderTest.java
@@ -12,19 +12,35 @@ public class AdjacencyMatrixBuilderTest {
 
     @Tested
     AdjacencyMatrixBuilder<MenuFrame, DefaultEdge> adjacencyMatrixBuilder;
+    @Tested
+    MenuFrame start;
+    @Tested
+    MenuFrame end;
 
     @Test
-    public void buildAdjacencyMatrixTest() {
+    public void buildAdjacencyMatrixWithNullLinesTest() {
         Graph<MenuFrame, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
-        MenuFrame start = new MenuFrame();
         start.setName("start");
-        MenuFrame end = new MenuFrame();
         end.setName("end");
         graph.addVertex(start);
         graph.addVertex(end);
         graph.addEdge(start, end);
         AdjacencyMatrix adjacencyMatrix = adjacencyMatrixBuilder.buildAdjacencyMatrix(graph);
         AdjacencyMatrix expected = new AdjacencyMatrix(new int[][]{{0, 1}, {0, 0}});
+        Assert.assertEquals(expected, adjacencyMatrix);
+    }
+
+    @Test
+    public void buildAdjacencyMatrixWithoutNullLinesTest() {
+        Graph<MenuFrame, DefaultEdge> graph = new DefaultDirectedGraph<>(DefaultEdge.class);
+        start.setName("start");
+        end.setName("end");
+        graph.addVertex(start);
+        graph.addVertex(end);
+        graph.addEdge(start, end);
+        graph.addEdge(end, start);
+        AdjacencyMatrix adjacencyMatrix = adjacencyMatrixBuilder.buildAdjacencyMatrix(graph);
+        AdjacencyMatrix expected = new AdjacencyMatrix(new int[][]{{0, 1}, {1, 0}});
         Assert.assertEquals(expected, adjacencyMatrix);
     }
 }
