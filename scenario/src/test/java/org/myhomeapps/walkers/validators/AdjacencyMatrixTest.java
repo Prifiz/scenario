bb08eed783de10186a3258a481ae8970e2f98e03
Squirrel994 <yarmol.olga@gmail.com>
2020-06-04 00:49:03 +0400

    add unit tests for DeadEndValidator

--- scenario/src/test/java/org/myhomeapps/walkers/validators/AdjacencyMatrixTest.java
+++ scenario/src/test/java/org/myhomeapps/walkers/validators/AdjacencyMatrixTest.java
@@ -6,13 +6,13 @@ import org.junit.Test;
 public class AdjacencyMatrixTest {
 
     @Test
-    public void getZerosLineIndicesPositiveTest() {
+    public void getNotEmptyZerosLineIndicesTest() {
         AdjacencyMatrix adjacencyMatrix = new AdjacencyMatrix(new int[][]{{0, 1}, {0, 0}});
         Assert.assertFalse(adjacencyMatrix.getZerosLineIndices().isEmpty());
     }
 
     @Test
-    public void getZerosLineIndicesNegativeTest() {
+    public void getEmptyZerosLineIndicesTest() {
         AdjacencyMatrix adjacencyMatrix = new AdjacencyMatrix(new int[][]{{0, 1}, {0, 1}});
         Assert.assertTrue(adjacencyMatrix.getZerosLineIndices().isEmpty());
     }
